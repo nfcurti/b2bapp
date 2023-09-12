c7a2d4998d8535af6cfba5d464ea66f3473c4d18
Juan Manuel <nicolascurti75@gmail.com>
2023-09-12 13:43:36 +0200

    fix

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/create', async (req, res) => {
                 'content-type': 'application/json',
                 'X-Shopify-Access-Token': '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 '
                 }})
-            .then(resJson => {console.log(resJson);res.redirect(`${host_url}/cart/${req.query.itemlist}?checkout[email]=${req.query.email}`)})
+            .then(resJson => {console.log(resJson)})
             .catch(err => console.log(err))
 })
 
