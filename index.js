8330f217be32865aa9112505863a8694dd3a2c5c
Juan Manuel <nicolascurti75@gmail.com>
2023-09-12 13:34:50 +0200

    fix

--- index.js
+++ index.js
@@ -5,65 +5,55 @@ const port = 3000
 
 var host_url = "https://b2b.isovoxbooth.com"
 
-app.get('/create', (req, res) => {
+app.get('/create', async (req, res) => {
     var tax_exempt = false
     
-    axios.get("https://abb5f7.myshopify.com/admin/api/2023-04/graphql.json",
-        {
-            headers: {
-            'content-type': 'application/json',
-            'X-Shopify-Access-Token': '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 '
-            },
-            method: "POST",
-            mode: "no-cors",
-            body: JSON.stringify({
+
+        axios
+            .post("https://abb5f7.myshopify.com/admin/api/2023-04/graphql.json", {
             query: `mutation customerCreate($input: CustomerInput!) {
-                        customerCreate(input: $input) {
-                        userErrors {
-                            field
-                            message
-                        }
-                        customer {
-                            id
-                            email
-                            phone
-                            taxExempt
-                            acceptsMarketing
-                            firstName
-                            lastName
-                            smsMarketingConsent {
-                            marketingState
-                            marketingOptInLevel
-                            }
-                            addresses {
-                            address1
-                            city
-                            country
-                            phone
-                            zip
-                            }
-                            note
-                        }
-                        }
-                } `, 
-            variables:{
-                    "input": {
-                    "email":req.query.email,
-                    "note": req.query.vatid,
-                    "taxExempt": tax_exempt, 
-                    
+                customerCreate(input: $input) {
+                userErrors {
+                    field
+                    message
                 }
-            }
-                })
-            })
-        .then((res) => (res.json())) 
-        .then((resJson)=>{
-            console.log(resJson);
-            res.redirect(`${host_url}/cart/${req.query.itemlist}?checkout[email]=${req.query.email}`)
-        })
-        .catch((error) => {
-            console.error(error); 
-        }); 
+                customer {
+                    id
+                    email
+                    phone
+                    taxExempt
+                    acceptsMarketing
+                    firstName
+                    lastName
+                    smsMarketingConsent {
+                    marketingState
+                    marketingOptInLevel
+                    }
+                    addresses {
+                    address1
+                    city
+                    country
+                    phone
+                    zip
+                    }
+                    note
+                }
+                }
+        } `,
+            variables: {
+                            "input": {
+                            "email":req.query.email,
+                            "note": req.query.vatid,
+                            "taxExempt": tax_exempt, 
+                            
+                        }
+                    },
+            }, {headers: {
+                'content-type': 'application/json',
+                'X-Shopify-Access-Token': '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 '
+                }})
+            .then(resJson => {console.log(resJson);res.redirect(`${host_url}/cart/${req.query.itemlist}?checkout[email]=${req.query.email}`)})
+            .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
